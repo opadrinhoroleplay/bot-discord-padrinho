7f0ed39d660c0174dba25f7991949b509d543182
VIRUXE <1616657+VIRUXE@users.noreply.github.com>
2022-11-28 00:21:40 +0000

    Update app.php

--- app.php
+++ app.php
@@ -709,21 +709,6 @@ $discord->listenCommand("rollcall", function (Interaction $interaction) use (&$r
 	$interaction->deleteOriginalResponse();
 });
 
-function slugify($string) {
-	$string = preg_replace('/[^\pL\d]+/u', '-', $string);
-	$string = iconv('utf-8', 'us-ascii//TRANSLIT', $string);
-	$string = preg_replace('/[^-\w]+/', '', $string);
-	$string = trim($string, '-');
-	$string = preg_replace('~-+~', '-', $string);
-	$string = strtolower($string);
-
-	if (empty($string)) {
-		return 'n-a';
-	}
-
-	return $string;
-}
-
 $discord->listenCommand('convite', function (Interaction $interaction) {
 	global $db;
 
