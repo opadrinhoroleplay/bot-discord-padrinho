a9e88cc53df1b6a9f20e1d5a284aab32f7db493e
VIRUXE <1616657+VIRUXE@users.noreply.github.com>
2022-12-03 20:12:51 +0000

    safe handling on FiveM::Status
    account for website being down layout changed

--- FiveM.php
+++ FiveM.php
@@ -7,18 +7,21 @@ class FiveM {
         Get the last rect element in the first svg element
         Get the fill attribute of that element of the rect
         If it's #05f4a7 then it's online, otherwise it's offline
-        */
-    static function Status(callable $callback = NULL): bool {
+    */
+    static function Status(callable $callback = null): bool|null {
         $doc = new DOMDocument();
         @$doc->loadHTML(file_get_contents("https://status.cfx.re/"));
         $xpath = new DOMXPath($doc,);
         $rect  = $xpath->query("//svg/rect[last()]")[0]; // Other elements are generated during runtime so this seemed the best bet
         $color = $rect->getAttribute("fill");
 
+        if(!$color) return null; // If the color is empty, then the website is probably down
+
         $status = $color === "#05f4a7" ? true : false;
 
+        // If the status has changed, call the callback
         if (self::$last_status !== $status) {
-            $callback($status);
+            if(self::$last_status != null) $callback($status); // Don't call the callback on the first run
             self::$last_status = $status;
         }
 
