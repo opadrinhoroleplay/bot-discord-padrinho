93739a707f1a9840c6f1f69436ce812c79ab3768
VIRUXE <1616657+VIRUXE@users.noreply.github.com>
2022-12-03 17:24:32 +0000

    namespace Utils

--- Utils.php
+++ Utils.php
@@ -1,47 +1,69 @@
 <?php
-function slugify($text)
-{
-	// replace non letter or digits by -
-	$text = preg_replace('~[^\pL\d]+~u', '-', $text);
 
-	// transliterate
-	$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-
-	// remove unwanted characters
-	$text = preg_replace('~[^-\w]+~', '', $text);
-
-	// trim
-	$text = trim($text, '-');
-
-	// remove duplicate -
-	$text = preg_replace('~-+~', '-', $text);
-
-	// lowercase
-	$text = strtolower($text);
-
-	if (empty($text)) return slugify(getInsult());
-
-	return $text;
+namespace Utils {
+	class Time
+	{
+		// Hour to milliseconds
+		static function hour(int $hours): int
+		{
+			return self::minute($hours * 60);
+		}
+		// Minute to milliseconds
+		static function minute(int $minutes): int
+		{
+			return $minutes * 60 * 1000;
+		}
+	}
 }
 
-function getRandomWord(bool $lowercase = true): string|null {
-	$words = file_get_contents("words.txt");
-	$words = explode("\n", $words);
+namespace Utils\Words {
+	function slugify($text)
+	{
+		// replace non letter or digits by -
+		$text = preg_replace('~[^\pL\d]+~u', '-', $text);
 
-	do {
-		$word = $words[rand(0, count($words)-1)];
-	} while (str_contains($word, "-") || strlen($word) >= 8); // Portuguese words can have dashes, so we'll just avoid them in this scenario
+		// transliterate
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
 
-	return $lowercase ? strtolower($word) : $word;
-}
+		// remove unwanted characters
+		$text = preg_replace('~[^-\w]+~', '', $text);
 
-function generateWhatThreeWords(): string {
-	return sprintf("%s-%s-%s", getRandomWord(), getRandomWord(), getRandomWord());
-}
+		// trim
+		$text = trim($text, '-');
 
-function getInsult() {
-	$insults = file_get_contents("insults.txt");
-	$insults = explode("\n", $insults);
+		// remove duplicate -
+		$text = preg_replace('~-+~', '-', $text);
 
-	return strtolower(trim($insults[rand(0, count($insults)-1)]));
+		// lowercase
+		$text = strtolower($text);
+
+		if (empty($text)) return slugify(getInsult());
+
+		return $text;
+	}
+
+	function getRandomWord(bool $lowercase = true): string|null
+	{
+		$words = file_get_contents("words.txt");
+		$words = explode("\n", $words);
+
+		do {
+			$word = $words[rand(0, count($words) - 1)];
+		} while (str_contains($word, "-") || strlen($word) >= 8); // Portuguese words can have dashes, so we'll just avoid them in this scenario
+
+		return $lowercase ? strtolower($word) : $word;
+	}
+
+	function generateWhatThreeWords(): string
+	{
+		return sprintf("%s-%s-%s", getRandomWord(), getRandomWord(), getRandomWord());
+	}
+
+	function getInsult()
+	{
+		$insults = file_get_contents("insults.txt");
+		$insults = explode("\n", $insults);
+
+		return strtolower(trim($insults[rand(0, count($insults) - 1)]));
+	}
 }
