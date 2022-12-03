7c9edc21a4fbaf07c7ad12bb1fc52011d75cb77f
VIRUXE <1616657+VIRUXE@users.noreply.github.com>
2022-12-03 20:18:18 +0000

    proper callback handling on FiveM::Status

--- FiveM.php
+++ FiveM.php
@@ -1,14 +1,15 @@
 <?php
 class FiveM {
-    static public $last_status = null;
-
+    
     /*
-        Scrape https://status.cfx.re/
-        Get the last rect element in the first svg element
-        Get the fill attribute of that element of the rect
-        If it's #05f4a7 then it's online, otherwise it's offline
+    Scrape https://status.cfx.re/
+    Get the last rect element in the first svg element
+    Get the fill attribute of that element of the rect
+    If it's #05f4a7 then it's online, otherwise it's offline
     */
     static function Status(callable $callback = null): bool|null {
+        static $last_status = null;
+
         $doc = new DOMDocument();
         @$doc->loadHTML(file_get_contents("https://status.cfx.re/"));
         $xpath = new DOMXPath($doc);
@@ -17,14 +18,14 @@ class FiveM {
 
         if(!$color) return null; // If the color is empty, then the website is probably down
 
-        $status = $color === "#05f4a7" ? true : false;
+        $curr_status = $color === "#05f4a7" ? true : false;
 
         // If the status has changed, call the callback
-        if (self::$last_status !== $status) {
-            if(self::$last_status != null) $callback($status); // Don't call the callback on the first run
-            self::$last_status = $status;
+        if ($curr_status !== $last_status) {
+            if($callback && $last_status != null) $callback($curr_status); // Only call if requested and if it's not the first run
+            $last_status = $curr_status;
         }
 
-        return $status;
+        return $curr_status;
     }
 }
